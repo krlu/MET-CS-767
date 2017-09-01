e30b6484e07d543cd6cc965d1e51e03461681f17
Kenneth Lu <krlu91@gmail.com>
2017-08-31 22:22:16 -0400

    intial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "MET_CS_767"
+
+version := "1.0"
+
+scalaVersion := "2.12.3"
+    
---
+++ src/main/scala/org/bu/met/MainApplication.scala
@@ -0,0 +1,8 @@
+package org.bu.met
+
+/**
+  * Created by Kenneth on 8/31/2017.
+  */
+class MainApplication {
+
+}
